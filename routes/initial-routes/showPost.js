ebb3786eb9189d1d793fa93aca1c26351f49561f
sk1es <144143057+sk1ees@users.noreply.github.com>
2024-08-21 22:50:45 +0530

    showPost-intregated

--- routes/initial-routes/showPost.js
+++ routes/initial-routes/showPost.js
@@ -1,6 +1,9 @@
-const showPost = (req, res) => {
-    res.send("showPost");
-  };
-  
-  module.exports = showPost;
-  
+const postModel = require("../../models/post");
+
+const showPost = async (req, res) => {
+  let post = await postModel.findOne({ _id: req.params.id });
+
+  res.render("showPost", { post });
+};
+
+module.exports = showPost;
