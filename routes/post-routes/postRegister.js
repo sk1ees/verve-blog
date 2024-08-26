d8bc48e4ad44382b1e76e469c28407466c57a0b0
sk1es <144143057+sk1ees@users.noreply.github.com>
2024-08-26 09:22:55 +0530

    error-handled

--- routes/post-routes/postRegister.js
+++ routes/post-routes/postRegister.js
@@ -4,7 +4,7 @@ const bcrypt = require("bcrypt");
 const postRegister = async (req, res) => {
   let { name, email, password, Gaming, Movies, Sports, Fashion } = req.body;
   let { path } = req.file;
-  console.log({ body: req.body, file: req.file });
+  // console.log({ body: req.body, file: req.file });
   let findUser = await userModel.findOne({ email });
   if (findUser) return res.send("user already exists");
   bcrypt.genSalt(10, (err, salt) => {
@@ -16,7 +16,7 @@ const postRegister = async (req, res) => {
         profileImage: path,
         tags: { Gaming, Movies, Sports, Fashion },
       });
-      console.log(user);
+      // console.log(user);
     });
   });
 
